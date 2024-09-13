73b959b1a9fc4e2f726dabe5709f7b6f65234183
ntnfit <ntnfit98@gmail.com>
2024-09-13 20:24:05 +0700

    update page detail

--- resources/views/blogdetail.blade.php
+++ resources/views/blogdetail.blade.php
@@ -26,7 +26,7 @@
                     <path fill="currentColor"
                         d="M2.64 11.917h16.591a.78.78 0 0 1 .769.792a.78.78 0 0 1-.769.791H.771c-.688 0-1.03-.857-.541-1.354L5.549 6.73a.754.754 0 0 1 1.087.006a.808.808 0 0 1-.005 1.119l-3.99 4.063Z" />
                 </svg>
-                <div class="text-xl font-medium">Blogs</div>
+                <div class="text-xl font-medium">{{ __('Blog') }}</div>
             </a>
             <div class="flex flex-wrap items-center gap-3 transition duration-300 will-change-transform"
                 :class="{
@@ -42,9 +42,13 @@
                     <x-articles.type-badge :type="$type" />
                 </div>
                 <div class="text-3xl font-extrabold leading-normal">
-                    {{ $article->title }}
+                    {{ App::getLocale() === 'vi' ? $article->title : $article->title_en ?? $article->title }}
+
                 </div>
-                <p>{{ $article->sub_title }}</p>
+
+                <p>
+                    {{ App::getLocale() === 'vi' ? $article->sub_title : $article->sub_title_en ?? $article->sub_title }}
+                </p>
                 <div class="pt-2">
                     <div class="text-base text-dolphin">
                         {{ $article->published_at->toFormattedDateString() }}
@@ -53,20 +57,15 @@
             </div>
             <div class="flex justify-center w-full sm:w-auto">
                 <img class="aspect-[16/9] w-full sm:w-10/12 rounded-xl bg-cover bg-center bg-no-repeat ring-1 ring-dawn-pink"
-                    {{-- src="{{ $article->featurePhoto }}" alt="{{ $article->photo_alt_text }}"> --}}
-                    src="https://images.ctfassets.net/h6goo9gw1hh6/5wl7KPvpM44dPJ3kwKfwTe/0eb029cd00424d1b1934d780f57bbc34/Aspect-Ration-Image-1to1.jpg?w=1600&h=1600&fl=progressive&q=70&fm=jpg" alt="{{ $article->photo_alt_text }}">
+                    src="{{ $article->featurePhoto }}" alt="{{ $article->photo_alt_text }}">
+                {{-- src="https://images.ctfassets.net/h6goo9gw1hh6/5wl7KPvpM44dPJ3kwKfwTe/0eb029cd00424d1b1934d780f57bbc34/Aspect-Ration-Image-1to1.jpg?w=1600&h=1600&fl=progressive&q=70&fm=jpg" --}}
+                {{-- alt="{{ $article->photo_alt_text }}"> --}}
             </div>
         </div>
 
         {{-- Categories --}}
         <div class="flex flex-wrap items-center gap-3.5 pt-6 mb-5">
-            @php
-            $article->categories[] = (object) ['name' => 'New Category'];
-            $article->categories[] = (object) ['name' => 'New Category 1'];
-            $article->categories[] = (object) ['name' => 'New Category 2'];
-            $article->categories[] = (object) ['name' => 'New Category 3'];
-            $article->categories[] = (object) ['name' => 'New Category 4'];
-            @endphp
+
             @foreach ($article->categories as $category)
                 <div class="select-none rounded-full bg-stone-200/50 px-5 py-2.5 text-sm">
                     <div class="text-sm">
@@ -98,7 +97,8 @@
                          prose-code:px-1.5 prose-code:py-0.5
                          prose-code:font-normal prose-code:before:hidden
                          prose-code:after:hidden [&_p]:before:hidden [&_p]:after:hidden  [&_code:not([class])]:bg-merino">
-                        {!! $article->body !!}
+                        {!! App::getLocale() === 'vi' ? $article->body : $article->body_en ?? $article->body !!}
+
                     </div>
                 </div>
             </div>
@@ -122,7 +122,7 @@
                     {{-- More From This Author --}}
                     <div class="mx-auto w-full">
                         <div class="text-lg font-extrabold">
-                            More from this author
+                            {{ __('More from this author') }}
                         </div>
 
                         <div class="w-full space-y-5 pt-7">
@@ -141,7 +141,7 @@
                                                     d="M9.153 5.408C10.42 3.136 11.053 2 12 2c.947 0 1.58 1.136 2.847 3.408l.328.588c.36.646.54.969.82 1.182c.28.213.63.292 1.33.45l.636.144c2.46.557 3.689.835 3.982 1.776c.292.94-.546 1.921-2.223 3.882l-.434.507c-.476.557-.715.836-.822 1.18c-.107.345-.071.717.001 1.46l.066.677c.253 2.617.38 3.925-.386 4.506c-.766.582-1.918.051-4.22-1.009l-.597-.274c-.654-.302-.981-.452-1.328-.452c-.347 0-.674.15-1.329.452l-.595.274c-2.303 1.06-3.455 1.59-4.22 1.01c-.767-.582-.64-1.89-.387-4.507l.066-.676c.072-.744.108-1.116 0-1.46c-.106-.345-.345-.624-.821-1.18l-.434-.508c-1.677-1.96-2.515-2.941-2.223-3.882c.293-.941 1.523-1.22 3.983-1.776l.636-.144c.699-.158 1.048-.237 1.329-.45c.28-.213.46-.536.82-1.182l.328-.588Z" />
                                             </svg>
                                             <div class="pt-0.5 text-sm font-medium text-dolphin">
-                                                {{ number_format(10) }}
+                                                {{ number_format(100) }}
                                             </div>
                                         </div>
                                     </div>
@@ -149,7 +149,8 @@
                                     {{-- Title --}}
                                     <div class="px-1 pb-1 pt-4 font-medium">
                                         <div class="line-clamp-2">
-                                            {{ $otherArticle->title }}
+                                            {{ App::getLocale() === 'vi' ? $otherArticle->title : $otherArticle->title_en ?? $otherArticle->title }}
+
                                         </div>
                                         <div class="pt-1 text-xs text-dolphin/80">
                                             {{ $otherArticle->published_at->diffForHumans() }}
