23588da6cd74bccea4b92f0ac3700971ce4cbb60
ntnfit <ntnfit98@gmail.com>
2024-09-15 17:22:32 +0700

    Refactor SEO metadata in home.blade.php

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,5 +1,12 @@
 @php
-    seo()->image(asset('images/og.jpg'));
+    seo()
+        ->image(asset('images/og.jpg'))
+        ->description('website chia sẻ kinh nghiệp về SAP B1/ERP')
+        ->title('SAP B1/ERP')
+        ->twitterImage(asset('images/og.jpg'))
+        ->url(url()->current())
+        ->set();
+
 @endphp
 <x-layouts.appclient>
     <x-home.hero />
