6c981c2baca73cb5203ed4b2991767af4c1f082d
harrysap <ntnfit98@gmail.com>
2024-11-08 15:08:33 +0700

    change about me to Giới thiệu

--- resources/views/test.blade.php
+++ resources/views/test.blade.php
@@ -53,7 +53,7 @@
                     <div class="d-none d-lg-flex"><div class="collapse navbar-collapse" id="navbarSupportedContent">
                             <ul class="navbar-nav me-auto mb-2 mb-lg-0">
                                 <li class="nav-item">
-                                    <a class="nav-link active" href="#about">About me</a>
+                                    <a class="nav-link active" href="#about">Giới thiệu</a>
                                 </li>
 
                                 <li class="nav-item">
