9350f5059e51efed2162aad0ff18ceaf5eb997cc
harrysap <nguyen03101998@gmail.com>
2025-07-31 17:54:43 +0700

    fix

--- resources/js/pages/BlogDetail.tsx
+++ resources/js/pages/BlogDetail.tsx
@@ -197,24 +197,23 @@ const BlogDetail: React.FC = () => {
                 { property: "og:image", content: metaImage },
                 { property: "og:url", content: metaUrl },
                 { property: "og:type", content: "article" },
-                { name: "twitter:card", content: "summary_large_image" },
                 { name: "twitter:title", content: metaTitle },
                 { name: "twitter:description", content: metaDescription },
                 { name: "twitter:image", content: metaImage },
             ];
 
-            // metaTags.forEach(({ name, property, content }) => {
-            //     let metaTag = document.querySelector(
-            //         `meta[${name ? "name" : "property"}="${name || property}"]`,
-            //     );
-            //     if (!metaTag) {
-            //         metaTag = document.createElement("meta");
-            //         if (name) metaTag.setAttribute("name", name);
-            //         if (property) metaTag.setAttribute("property", property);
-            //         document.head.appendChild(metaTag);
-            //     }
-            //     metaTag.setAttribute("content", content);
-            // });
+            metaTags.forEach(({ name, property, content }) => {
+                let metaTag = document.querySelector(
+                    `meta[${name ? "name" : "property"}="${name || property}"]`,
+                );
+                if (!metaTag) {
+                    metaTag = document.createElement("meta");
+                    if (name) metaTag.setAttribute("name", name);
+                    if (property) metaTag.setAttribute("property", property);
+                    document.head.appendChild(metaTag);
+                }
+                metaTag.setAttribute("content", content);
+            });
         }
     }, [blog]);
 
@@ -448,16 +447,7 @@ const BlogDetail: React.FC = () => {
         <div className="min-h-screen bg-white dark:bg-gray-900 transition-colors duration-500">
             <Head title={`${blog.title} | My Blog`}>
                 <meta name="description" content={blog.title || ''} />
-                <meta name="twitter:card" content="summary_large_image" />
-                <meta property="og:type" content="article" />
-                <meta name="twitter:card" content="summary_large_image" />
-                <meta name="twitter:title" content={`${blog.title}`} />
-                <meta name="twitter:description" content={`${blog.thumbnail_url}` ||
-                    blog.featured_image ||
-                    "/default-image.jpg"} />
-                <meta name="twitter:image" content={`${blog.thumbnail_url}` ||
-                    blog.featured_image ||
-                    "/default-image.jpg"} />
+                <meta name="og:type" content="type" />
                 <meta name="google-adsense-account" content="ca-pub-6568899988616854" />
                 <script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-6568899988616854" crossOrigin="anonymous"></script>
                 <link rel="icon" type="image/x-icon" href="/favicon/favicon-32x32" />
