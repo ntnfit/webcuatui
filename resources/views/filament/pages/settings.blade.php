782207af27a93c6358fa021a207ab4226bf6214f
ntnfit <ntnfit98@gmail.com>
2026-03-13 11:27:24 +0700

    feat: implement initial settings page with a save form

--- resources/views/filament/pages/settings.blade.php
+++ resources/views/filament/pages/settings.blade.php
@@ -1,10 +1,10 @@
 <x-filament-panels::page>
-    <x-filament-panels::form wire:submit="create">
+    <form  wire:submit="create">
         {{ $this->form }}
         <div>
             <x-filament::button type="submit" size="sm">
                 Save
             </x-filament::button>
         </div>
-    </x-filament-panels::form>
+    </form>
 </x-filament-panels::page>
