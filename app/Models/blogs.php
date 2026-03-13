772e51cdcc264260765cbed97bfffe52b459dd61
ntnfit <ntnfit98@gmail.com>
2026-03-13 11:32:28 +0700

    remove profle opton tiptap

--- app/Models/blogs.php
+++ app/Models/blogs.php
@@ -186,7 +186,6 @@ class blogs extends Model
                                         ->columnSpanFull(),
                                     RichEditor::make('body_en')
                                         ->label('Body English')
-                                        ->profile('default')
                                         ->disableFloatingMenus()
                                         ->extraInputAttributes(['style' => 'max-height: 30rem; min-height: 24rem', 'source_code_editor'])
                                         ->columnSpanFull(),
